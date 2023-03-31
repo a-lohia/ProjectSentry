47d03ca8c69057b883d726ff4c8272a4e3e0675a
Arya Lohia <96156566+a-lohia@users.noreply.github.com>
2023-03-31 15:54:01 -0400

    did some work on the UFPR Dataset containing class

--- src/data/car_plate_dataset.py
+++ src/data/car_plate_dataset.py
@@ -1,17 +1,38 @@
 from pathlib import Path
-from typing import Union, List
+from typing import Union, List, Dict
+import os
 
 
 class UFPRPlateDataset:
+    """
+    Class to make accessing data from UFPRPlateDataset easy
+    """
 
     def __init__(self, dataset_dir: Path):
         # Dictionary[path to image, tuple(licence plate number, plate bounding box (x_min, y_min, x_max, y_max))
+        self.paths = {
+            "testing": os.path.join(dataset_dir, "/testing"),
+            "training": os.path.join(dataset_dir, "/training"),
+            "validation": os.path.join(dataset_dir, "/validation")
+        }
 
 
-    def read_image_txt(self, image_id: int, ) -> Union[List, tuple]:
+    def _setup(self):
+        """Run once on initialization of the database object"""
 
-        with open("track0091[01].txt", "r") as track0091_01_txt:
-            txt = track0091_01_txt.readlines()
+        # dictionary which has values of lists of sets of pictures
+        image_sets = {j: os.listdir(i) for i, j in zip(self.paths.values(), self.paths.keys())}
+
+
+
+    def read_image_txt(self, image_id: str, ) -> Union[List, tuple]:
+        """
+        :param image_id: in the format of: "track####[##].txt"
+        :return: Union[List(Licence Plate Characters), Plate_Boundary: Tuple(Xmin, Ymin, Xmax, Ymax)]
+        """
+
+        with open(image_id, "r") as txt_file:
+            txt = txt_file.readlines()
 
             lp = txt[6].replace("plate: ", "")  # line 6 in the txt has licence plate number
             plate = txt[7]  # line 7 has bounding box location for licence plate
